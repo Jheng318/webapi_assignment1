9ccd583d22c84c526f984e1fa0b06a6a4f56f79e
Jheng318 <jhchan0514@gmail.com>
2025-11-17 15:10:53 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const server = require("./Chan_Jia_Hong_EGL301L2");
 const database = require("./database");
 
-// 1 function (filter the posts by a given title)
-server.filterPostsByTitle("What is the cosmere?"); // test filter function that will return a valid posts
+// 1 function (search the posts by a given title)
+server.searchPostsByTitle("What is the cosmere?"); // test filter function that will return a valid posts
 console.log("\n");
-server.filterPostsByTitle(); //  test exception to handle no title being found
+server.searchPostsByTitle(); //  test exception to handle no title being found
 console.log("\n");
-server.filterPostsByTitle("nani"); // test exception that handles a posts that is not found with the provided title in the database
+server.searchPostsByTitle("nani"); // test exception that handles a posts that is not found with the provided title in the database
 
 // 2 function (to create a new posts)
 
